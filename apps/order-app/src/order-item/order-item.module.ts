02d9cd493a095343171a9bc4e09c915348826b35
HaoNgo232 <ngogiahao2018@gmail.com>
2025-11-02 19:36:29 +0700

    refactor: remove OrderItemController and associated tests to streamline order item handling

--- apps/order-app/src/order-item/order-item.module.ts
+++ apps/order-app/src/order-item/order-item.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { OrderItemService } from '@order-app/order-item/order-item.service';
-import { OrderItemController } from '@order-app/order-item/order-item.controller';
 import { PrismaService } from '@order-app/prisma/prisma.service';
 
 @Module({
-  controllers: [OrderItemController],
+  controllers: [],
   providers: [OrderItemService, PrismaService],
   exports: [OrderItemService],
 })
